c08e827ef6fce0116a8a9bc63dd61952873016a9
DexBurns25 <71415601+DexBurns25@users.noreply.github.com>
2020-12-03 19:23:05 -0500

    more commits

---
+++ tableDate.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+        customerName: "Brian",
+        customerEmail: "Brian15@gmail.com",
+        cutomerID: "Brian15",
+        phoneNumber: "212-444-5342",
+    }
+    ];
+    
+    module.exports = tableArray;
