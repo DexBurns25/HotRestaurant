6f40e9c45c6fd99aeab66cfd594edd2e410f03fc
DexBurns25 <71415601+DexBurns25@users.noreply.github.com>
2020-12-03 19:24:04 -0500

    Add files via upload

---
+++ waitinglistData.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+        customerName: "Rubin",
+        customerEmail: "Rubin@yahoo.com",
+        customerID: "Ruby12",
+        phoneNumber: "222-222-5468"
+    }
+    ];
+    
+    module.exports = waitingArray;
